cf897bf416159bac067f39296e973681aa19ca64
Erik Präntare <erik.prantare@gmail.com>
2020-03-15 10:45:10 +0100

    Remove d suffix for double literals

--- terrainMeshLoader.cpp
+++ terrainMeshLoader.cpp
@@ -61,8 +61,8 @@ mesh(double _x, double _z, double _scale)
     for(int x = 0; x < TerrainMeshLoader::granularity; x++)
     for(int z = 0; z < TerrainMeshLoader::granularity; z++) {
         double discScale = std::pow(2.0, int(log2(_scale)));
-        double discX = int(_x*discScale/8)/discScale*8;
-        double discZ = int(_z*discScale/8)/discScale*8;
+        double discX = int(_x*discScale/8.0)/discScale*8.0;
+        double discZ = int(_z*discScale/8.0)/discScale*8.0;
         double xPos = 
             (x/(TerrainMeshLoader::granularity/32.0)-16.0)/discScale + discX;
         double zPos = 
@@ -149,25 +149,25 @@ TerrainMeshLoader::heightAt(const std::complex<double>& c)
     std::complex<double> dz(0.0, 0.0);
     
     //main cardioid check
-    double q = pow(c.real()-0.25d, 2.0d) + c.imag()*c.imag();
-    if(q*(q+(c.real()-0.25d)) < 0.25d*c.imag()*c.imag()) {
-        return 0.0d;
+    double q = pow(c.real()-0.25, 2.0) + c.imag()*c.imag();
+    if(q*(q+(c.real()-0.25)) < 0.25*c.imag()*c.imag()) {
+        return 0.0;
     }
 
     //period-2 bulb check
-    if((c.real()+1.0d)*c.real()+1.0d + c.imag()*c.imag() < 0.625d) {
-        return 0.0d;
+    if((c.real()+1.0)*c.real()+1.0 + c.imag()*c.imag() < 0.625) {
+        return 0.0;
     }
 
     int i = 0;
     for(; i < iterations; ++i) {
-        dz = 2.0d*z*dz + 1.0d;
+        dz = 2.0*z*dz + 1.0;
         z = z*z + c;
 
         if(std::abs(z) > 256) {
             double r = std::abs(z);
             double dr = std::abs(dz);
-            double de = 2.0d*r*std::log(r)/dr; //estimated distance from set
+            double de = 2.0*r*std::log(r)/dr; //estimated distance from set
             return de;
         }
     }
