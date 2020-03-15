c3d1d30010093f183bbae89e647c4c0a2913715a
ElderTwig <pqfrml@gmail.com>
2020-03-15 01:58:27 +0100

    Make terrainMeshLoader.cpp compliant with the language standard once again

--- terrainMeshLoader.cpp
+++ terrainMeshLoader.cpp
@@ -149,25 +149,25 @@ TerrainMeshLoader::heightAt(const std::complex<double>& c)
     std::complex<double> dz(0.0, 0.0);
     
     //main cardioid check
-    double q = pow(c.real()-0.25d, 2.0d) + c.imag()*c.imag();
-    if(q*(q+(c.real()-0.25d)) < 0.25d*c.imag()*c.imag()) {
-        return 0.0d;
+    double q = pow(c.real()-0.25, 2.0) + c.imag()*c.imag();
+    if(q*(q+(c.real()-0.25)) < 0.25*c.imag()*c.imag()) {
+        return 0.0;
     }
 
     //period-2 bulb check
-    if((c.real()+1.0d)*c.real()+1.0d + c.imag()*c.imag() < 0.625d) {
-        return 0.0d;
+    if((c.real()+1.0)*c.real()+1.0 + c.imag()*c.imag() < 0.625) {
+        return 0.0;
     }
 
     int i = 0;
     for(; i < iterations; ++i) {
-        dz = 2.0d*z*dz + 1.0d;
+        dz = 2.0*z*dz + 1.0;
         z = z*z + c;
 
         if(std::abs(z) > 256) {
             double r = std::abs(z);
             double dr = std::abs(dz);
-            double de = 2.0d*r*std::log(r)/dr; //estimated distance from set
+            double de = 2.0*r*std::log(r)/dr; //estimated distance from set
             return de;
         }
     }
