f9caaa9ce2ca5f0c64001eb9cbd85121a7bec228
ElderTwig <pqfrml@gmail.com>
2020-03-22 23:03:38 +0100

    Add version check to clang-format call

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,4 +8,10 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-clang-format-9 -i ${DIR}/src/*
+if [ "$(clang-format-9 --version 2> /dev/null | grep -o '9.0')" == "9.0" ];
+then
+  clang-format-9 -i ${DIR}/src/*
+elif [ "$(clang-format --version | grep -o '9.0')" == "9.0" ];
+then
+  clang-format -i ${DIR}/src/*
+fi
