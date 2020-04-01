7309c90ed9c37fac69a18e9b88d52a3d737232a8
ElderTwig <pqfrml@gmail.com>
2020-04-01 11:01:10 +0200

    Add callback support for Config

--- src/config.h
+++ src/config.h
@@ -3,9 +3,13 @@
 
 #include <type_traits>
 #include <map>
+#include <vector>
 #include <any>
+#include <functional>
 
 class Config {
+    using FunctionSig = std::function<void(std::any const&)>;
+
 public:
     Config() = default;
 
@@ -19,6 +23,13 @@ public:
     set(T newValue)
     {
         m_settings[Setting::uid] = std::any(std::move(newValue));
+        std::any const& value    = m_settings[Setting::uid];
+
+        std::vector<FunctionSig> const& callbacks = m_callbacks[Setting::uid];
+        std::for_each(
+                std::cbegin(callbacks),
+                std::cend(callbacks),
+                [&value](FunctionSig const& func) { func(value); });
     }
 
     template<
@@ -31,8 +42,21 @@ public:
         return std::any_cast<ReturnType>(m_settings[Setting::uid]);
     }
 
+    template<
+            typename Setting,
+            typename T = typename Setting::type,
+            typename   = typename std::enable_if_t<
+                    Setting::isSetting
+                    && std::is_same_v<typename Setting::type, T>>>
+    void
+    subscribe_with(FunctionSig const callback)
+    {
+        m_callbacks[Setting::uid].push_back(std::move(callback));
+    }
+
 private:
     std::map<int, std::any> m_settings;
+    std::map<int, std::vector<FunctionSig>> m_callbacks;
 };
 
 #endif    // CONFIG_H
