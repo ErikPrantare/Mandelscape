39d10b1b98143a5f1cdba601dbbb7f944fc2a482
Erik Präntare <erik.prantare@gmail.com>
2020-04-11 19:54:00 +0200

    Move utility functions to separate namespace

--- src/utils.h
+++ src/utils.h
@@ -5,14 +5,19 @@
 #include <fstream>
 #include <future>
 
+namespace util {
+
 std::string
 readFile(std::string const& filePath);
 
+// CPP20 https://en.cppreference.com/w/cpp/experimental/future/is_ready
 template<typename T>
 static bool
 isDone(const std::future<T>& f)
 {
     return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 }
+
+}    // namespace util
 
 #endif    // MANDELLANDSCAPE_UTILS_H
