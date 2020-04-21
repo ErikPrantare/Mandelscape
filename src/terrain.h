9699aa14091c278d888b0e1b3df1f0e094a94312
Erik Präntare <erik.prantare@gmail.com>
2020-04-21 19:54:26 +0200

    Fix typo that broke build

--- src/terrain.h
+++ src/terrain.h
@@ -35,7 +35,7 @@ private:
 
     static int constexpr granularity    = 400;
     int m_iterations                    = 100;
-    static int constexpruploadChunkSize = 90'000;
+    static int constexpr uploadChunkSize = 90'000;
 
     std::function<void(double, double)> m_setMeshOffset;
 
