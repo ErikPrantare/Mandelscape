be6bddf983ac0ab471a95b766b8286d2f672d30e
ElderTwig <pqfrml@gmail.com>
2020-05-23 00:47:41 +0200

    Clean up cmake modules

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,47 +1,13 @@
 find_path(GLFW_INCLUDE_DIR
     NAMES
-        GLFW/glfw3.h
-    PATHS
-        ${GLFW_LOCATION}/include
-        $ENV{GLFW_LOCATION}/include
-        $ENV{PROGRAMFILES}/GLFW/include
-        ${OPENGL_INCLUDE_DIR}
-        /usr/include
-        /usr/local/include
-        /usr/include/GL)
+        GLFW/glfw3.h)
 
-find_library(GLFW_glfw_LIBRARY
+find_library(GLFW_LIBRARIES
     NAMES
         glfw3
-        glfw
-    PATHS
-        ${GLFW_LOCATION}/lib
-        ${GLFW_LOCATION}/lib/x64
-        $ENV{GLFW_LOCATION}/lib
-        $ENV{GLFW_LOCATION}/lib/x64
-        $ENV{PROGRAMFILES}/GLFW/lib
-        ${OPENGL_LIBRARY_DIR}
-        /usr/lib
-        /usr/local/lib
-        /usr/lib64
-        /usr/local/lib64)
+        glfw)
 
-if(GLFW_INCLUDE_DIR AND GLFW_glfw_LIBRARY)
-  if(UNIX)
-    find_package(X11 REQUIRED)
-  endif()
-
-  set(GLFW_INCLUDE_DIR
-      ${GLFW_INCLUDE_DIR}
-      ${X11_INCLUDE_DIR})
-
-  set(GLFW_LIBRARIES
-      ${GLFW_glfw_LIBRARY}
-      ${X11_LIBRARY})
-
-  set(GLFW_LIBRARY
-      ${GLFW_LIBRARIES})
-endif()
+mark_as_advanced(GLFW_INCLUDE_DIR GLFW_glfw_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 
@@ -50,7 +16,12 @@ find_package_handle_standard_args(GLFW
         GLFW_INCLUDE_DIR 
         GLFW_LIBRARIES)
 
-mark_as_advanced(
-    GLFW_INCLUDE_DIR
-    GLFW_glfw_LIBRARY
-    GLFW_LIBRARIES)
+if(GLFW_FOUND AND NOT TARGET glfw::glfw)
+  add_library(glfw::glfw INTERFACE IMPORTED)
+  
+  target_include_directories(glfw::glfw
+      INTERFACE ${GLFW_INCLUDE_DIR})
+
+  target_link_libraries(glfw::glfw
+      INTERFACE ${GLFW_LIBRARIES})
+endif()
--- cmake/Findstb.cmake
+++ cmake/Findstb.cmake
@@ -1,27 +1,13 @@
-find_path(STB_INCLUDE_DIR
+find_path(STB_STB_INCLUDE_DIR
     NAMES
-        stb_image.h
-    PATHS
-        ${STB_LOCATION}
-        $ENV{STB_LOCATION}
-        $ENV{PROGRAMFILES}/stb
-        /usr/include
-        /usr/local/include
-        /usr/include/stb
-        /usr/local/include/stb)
+        stb/stb_image.h)
+
+find_path(STB_INCLUDE_DIR ${STB_STB_INCLUDE_DIR}/stb)
+
+mark_as_advanced(STB_STB_INCLUDE_DIR STB_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(stb
     REQUIRED_VARS 
         STB_INCLUDE_DIR)
-
-if(NOT TARGET stb)
-  add_library(stb INTERFACE IMPORTED)
-
-  target_include_directories(stb
-      INTERFACE ${STB_INCLUDE_DIR})
-endif()
-
-mark_as_advanced(
-    STB_INCLUDE_DIR)
