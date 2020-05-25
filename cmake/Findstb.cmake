d650cc78b8cdcf23bd3cbd44340f5cd7651a3f09
ElderTwig <pqfrml@gmail.com>
2020-05-25 19:12:53 +0200

    Fix Findstb for real

--- cmake/Findstb.cmake
+++ cmake/Findstb.cmake
@@ -1,15 +1,15 @@
-find_path(STB_STB_INCLUDE_DIR
+find_path(stb_stb_INCLUDE_DIR
     NAMES
         stb/stb_image.h)
 
-if(STB_STB_INCLUDE_DIR)
-    set(STB_INCLUDE_DIR ${STB_STB_INCLUDE_DIR}/stb)
+if(stb_stb_INCLUDE_DIR)
+    set(stb_INCLUDE_DIR ${stb_stb_INCLUDE_DIR}/stb)
 endif()
 
-mark_as_advanced(STB_STB_INCLUDE_DIR STB_INCLUDE_DIR)
+mark_as_advanced(stb_stb_INCLUDE_DIR stb_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(stb
     REQUIRED_VARS 
-        STB_INCLUDE_DIR)
+        stb_INCLUDE_DIR)
