44a9533158762f8824704e8e43bc0292052b4bdf
ElderTwig <pqfrml@gmail.com>
2020-07-22 13:53:39 +0200

    Add tests for PlayerController

--- tests/playerController-tests.hpp
+++ tests/playerController-tests.hpp
@@ -5,4 +5,58 @@
 
 #include <catch2/catch.hpp>
 
+#include <functional>
+
+struct Controller {
+    std::reference_wrapper<bool> handleEventFlag;
+    std::reference_wrapper<bool> updateFlag;
+
+    Controller(bool& _handleEventFlag, bool& _updateFlag) :
+                handleEventFlag{_handleEventFlag},
+                updateFlag{_updateFlag}
+    {
+        REQUIRE(handleEventFlag.get() == false);
+        REQUIRE(updateFlag.get() == false);
+    }
+
+    auto
+    handleEvent(Event const&) -> void
+    {
+        handleEventFlag.get() = !handleEventFlag.get();
+    }
+
+    auto
+    update(Player&, double) -> void
+    {
+        updateFlag.get() = !updateFlag.get();
+    }
+};
+
+TEST_CASE(
+        "PlayerController calls erased controllers implementations",
+        "[PlayerController]")
+{
+    auto handleEventFlag = false;
+    auto updateFlag      = false;
+
+    auto playerController =
+            PlayerController{Controller{handleEventFlag, updateFlag}};
+
+    SECTION("PlayerController::handleEvent")
+    {
+        auto const dummyEvent = Event{KeyDown{}};
+        playerController.handleEvent(dummyEvent);
+        REQUIRE(handleEventFlag);
+        REQUIRE_FALSE(updateFlag);
+    }
+
+    SECTION("PlayerController::update")
+    {
+        auto dummyPlayer = Player{};
+        playerController.update(dummyPlayer, {});
+        REQUIRE_FALSE(handleEventFlag);
+        REQUIRE(updateFlag);
+    }
+}
+
 #endif    // MANDELLANDSCAPE_PLAYER_CONTROLLER_TESTS_HPP
